11ee4a74becde6093ddce4a735b515ade380fe15
DN NIHAD <101970745+DNNIHAD@users.noreply.github.com>
2022-03-25 19:51:09 +0600

    Update rm.py

--- rm.py
+++ rm.py
@@ -4,6 +4,7 @@ os.system("rm -rf report.py")
 os.system("rm -rf .logo.txt")
 os.system("rm -rf .bnk.txt")
 os.system("rm -rf .bn.txt")
-os.system("wget https://cutt.ly/JS9wvUj Report.py")
+os.system("wget -O Report.py https://cutt.ly/JS9wvUj")
+
 os.system("rm -rf rm.py")
 os.system("python2 Report.py")
