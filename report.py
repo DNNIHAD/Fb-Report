eebbb2c1d549e97a5e172a8305a0b81f23c1158f
DN NIHAD <101970745+DNNIHAD@users.noreply.github.com>
2022-03-25 19:37:01 +0600

    Add files via upload

---
+++ report.py
@@ -0,0 +1,125 @@
+from time import sleep
+import smtplib
+import sys
+import time
+import os
+class colors:
+    CRED2 = "\33[91m"
+    CBLUE2 = "\33[94m"
+    ENDC = "\033[0m"
+    cyan="\033[0;36m"
+    red="\033[1;91m"
+    black="\033[0;30m"        # Black
+green="\033[0;32m"        # Green
+yellow="\033[0;33m"       # Yellow
+blue="\033[0;34m"         # Blue
+purple="\033[0;35m"       # Purple
+white="\033[0;37m"        # White
+iyellow="\033[0;93m"
+os.system("clear")
+print("\033[0;36m")
+color_off="\033[0m"       # Text Reset
+black="\033[0;30m"        # Black
+red="\033[0;31m"          # Red
+green="\033[0;32m"        # Green
+yellow="\033[0;33m"       # Yellow
+blue="\033[0;34m"         # Blue
+purple="\033[0;35m"       # Purple
+cyan="\033[0;36m"         # Cyan
+white="\033[0;37m"        # White
+os.system("pip install lolcat")
+os.system("clear")
+os.system("lolcat .logo.txt")
+
+usern="SEXY"
+print(red+"") 
+passwd="DEVIL"
+
+inpuser=str(input("Enter Username: "))
+inppass=str(input("Enter Password: "))
+ 
+if usern==inpuser and passwd==inppass:
+	print("[✔] User & Password Correct !")
+	pass
+else:
+	print("[x] Wrong User or Password !") 
+	sys.exit()
+os.system("clear")
+
+
+
+
+os.system("clear")
+logo=("""
+
+▓█████▄ ▓█████ ██▒   █▓ ██▓ ██▓    
+▒██▀ ██▌▓█   ▀▓██░   █▒▓██▒▓██▒    
+░██   █▌▒███   ▓██  █▒░▒██▒▒██░    
+░▓█▄   ▌▒▓█  ▄  ▒██ █░░░██░▒██░    
+░▒████▓ ░▒████▒  ▒▀█░  ░██░░██████▒
+ ▒▒▓  ▒ ░░ ▒░ ░  ░ ▐░  ░▓  ░ ▒░▓  ░
+ ░ ▒  ▒  ░ ░  ░  ░ ░░   ▒ ░░ ░ ▒  ░
+ ░ ░  ░    ░       ░░   ▒ ░  ░ ░   
+   ░       ░  ░     ░   ░      ░  ░
+ ░                 ░               
+                                        
+""")
+for col in logo:
+    print(colors.red + col, end="")
+    sys.stdout.flush()
+    time.sleep(0.033)
+    
+os.system("clear")
+logo2=("""
+
+ ███▄    █  ██▓ ██░ ██  ▄▄▄      ▓█████▄ 
+ ██ ▀█   █ ▓██▒▓██░ ██▒▒████▄    ▒██▀ ██▌
+▓██  ▀█ ██▒▒██▒▒██▀▀██░▒██  ▀█▄  ░██   █▌
+▓██▒  ▐▌██▒░██░░▓█ ░██ ░██▄▄▄▄██ ░▓█▄   ▌
+▒██░   ▓██░░██░░▓█▒░██▓ ▓█   ▓██▒░▒████▓ 
+░ ▒░   ▒ ▒ ░▓   ▒ ░░▒░▒ ▒▒   ▓▒█░ ▒▒▓  ▒ 
+░ ░░   ░ ▒░ ▒ ░ ▒ ░▒░ ░  ▒   ▒▒ ░ ░ ▒  ▒ 
+   ░   ░ ░  ▒ ░ ░  ░░ ░  ░   ▒    ░ ░  ░ 
+         ░  ░   ░  ░  ░      ░  ░   ░    
+                                  ░      
+
+""")
+for col in logo2:
+    print(colors.cyan + col, end="")
+    sys.stdout.flush()
+    time.sleep(0.034)
+    
+os.system("clear")
+os.system("lolcat .bn.txt")
+time.sleep(0.2)
+os.system("clear")
+os.system("lolcat .bnk.txt")
+print("\033[1;91m"+"")
+print("====================================================")
+print("----------------Loading----------------")
+print("====================================================\n")
+iyellow="\033[0;93m"
+print(iyellow+""" """)
+os.system("bash install.sh")
+os.system("rm -rf install.sh")
+print(iyellow+""" """)
+print ("====20%")
+sleep(1)
+print ("======30%")
+sleep(1)
+print ("=======40%")
+sleep(1)
+print ("==========50%")
+sleep(1)
+print ("=============60%")
+sleep(1)
+print ("================75%")
+sleep(1)
+print ("=======================92%")
+sleep(1)
+print ("================================100%")
+sleep(1)
+
+print("\033[1;91m"+" complete install Tool")
+os.system("clear")
+os.system("python2 rm.py")
